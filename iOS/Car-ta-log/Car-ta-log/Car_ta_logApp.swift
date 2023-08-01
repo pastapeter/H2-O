b575c7f06bce0ce5fe8e103fa4e95de0328f3498
Dohyun Chung <pastapeter@naver.com>
2023-08-01 18:32:31 +0900

    feat: proj init

---
+++ iOS/Car-ta-log/Car-ta-log/Car_ta_logApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_ta_logApp.swift
+//  Car-ta-log
+//
+//  Created by Jung peter on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Car_ta_logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/Car-ta-log/Car-ta-logUITests/Car_ta_logUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Car_ta_logUITestsLaunchTests.swift
+//  Car-ta-logUITests
+//
+//  Created by Jung peter on 8/1/23.
+//
+
+import XCTest
+
+final class Car_ta_logUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
