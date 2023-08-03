91b333c20a00ead9e3fe4660609694d554234bc6
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-08-03 15:10:37 +0900

    feature: backend setting (#40)
    * Update new-feature.md
    
    * docs: gitignore 설정
    
    * feat: 스프링부트 프로젝트 초기화
    
    * feat: ping API 구현
    
    * docs: .idea 관련 gitignore 설정 추가
    
    ---------
    
    Co-authored-by: DOHYUN CHUNG <69891604+pastapeter@users.noreply.github.com>

---
+++ backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
@@ -0,0 +1,23 @@
+package com.h2o.h2oServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@RestController
+public class H2oServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2oServerApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public ResponseEntity ping() {
+		return ResponseEntity.ok("pong");
+	}
+}
---
+++ backend/src/test/java/com/h2o/h2oServer/H2oServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2o.h2oServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2oServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
