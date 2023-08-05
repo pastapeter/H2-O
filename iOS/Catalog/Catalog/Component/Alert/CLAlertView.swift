e2ca4d6ebf453007257cd9c3415759b5eaf14609
Dohyun Chung <pastapeter@naver.com>
2023-08-05 23:15:50 +0900

    feat: CLAlertView View 그리기

---
+++ iOS/Catalog/Catalog/Component/Alert/CLAlertView.swift
@@ -0,0 +1,43 @@
+//
+//  CLAlert.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/5/23.
+//
+
+import SwiftUI
+
+struct CLAlertView: View {
+
+  var message: String?
+  var cancelAction: () -> Void
+  var submitAction: () -> Void
+
+    var body: some View {
+      VStack(alignment: .center) {
+        Spacer().frame(height: 50)
+          .multilineTextAlignment(.center)
+          Text(message ?? "")
+          .multilineTextAlignment(.center)
+        Spacer().frame(height: 40)
+        CLDualChoiceButton(leftText: "취소", rightText: "종료", height: 52, leftAction: cancelAction, rightAction: submitAction)
+      }
+      .padding(.horizontal, 20)
+      
+    }
+}
+
+extension CLAlertView {
+  @ViewBuilder
+  static func build(message: String?, cancelAction: @escaping ()->Void, submitAction: @escaping () -> Void) -> some View {
+    CLAlertView(message: "", cancelAction: cancelAction, submitAction: submitAction)
+  }
+}
+
+struct CLAlertView_Previews: PreviewProvider {
+    static var previews: some View {
+      CLAlertView(message: "내 차 만들기를 종료하시겠습니까?\n지금 서비스 종료 시 저장되지 않습니다.", cancelAction: { }, submitAction: { })
+    }
+}
+
+
