f6c10781ad5521b9890fc5b3a24a1a093bfb6bcf
ori5r2 <ori5ri512@gmail.com>
2023-08-06 21:19:00 +0900

    feat: cta_ctype 버튼 index 파일에 추가

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -2,6 +2,6 @@ export { Icon } from './Icon';
 export { Header } from './Header';
 export { PriceStaticBar } from './PriceStaticBar';
 export { HMGTag } from './HMGTag';
-export { CTAButton, OptionTagButton, ImageButton, PriceSummaryButton } from './Button';
+export { CTAButton, CTACTypeButton, OptionTagButton, ImageButton, PriceSummaryButton } from './Button';
 export { Portal } from './Portal';
 export { Dimmed } from './Dimmed';
