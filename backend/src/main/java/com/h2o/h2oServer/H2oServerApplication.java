d35963520696e5420d614fa9b0e5fc99374a9093
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-08-07 17:38:22 +0900

    feature/#61/mysql 서버 연결 (#63)
    * <docs> 백엔드 서버 CI workflow 작성
    
    * Update README.md
    
    * Update backend-ci.yml
    
    * test: github actions CI 테스트를 위한 실패하는 코드 추가
    
    * docs: IDE 설정 파일 gitignore 추가
    
    * feat: 백엔드 CI 워크플로우 수정
    
    * feat: db connection 관련 의존성 추가
    
    * feat: 로컬, ci 환경에서 기본적으로 h2 db를 datasource로 설정
    
    * feat: 도커 이미지 빌드 시 mysql 서버를 datasource로 적용되도록 수정

--- backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
+++ backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @RestController
 public class H2oServerApplication {
 
