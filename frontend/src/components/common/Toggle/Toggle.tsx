ec1da0de9d5079c52be4092b3886da3ac0d1a5a8
ori5r2 <ori5ri512@gmail.com>
2023-08-07 15:56:12 +0900

    feat: 토글 슬라이드 애니메이션 구현

--- frontend/src/components/common/Toggle/Toggle.tsx
+++ frontend/src/components/common/Toggle/Toggle.tsx
@@ -1,4 +1,5 @@
 import { ChangeEventHandler, Dispatch, SetStateAction } from 'react';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 interface Props {
@@ -13,14 +14,23 @@ function Toggle({ isChecked, size, setIsChecked }: Props) {
 
   return (
     <ToggleContainer size={size}>
-      <ToggleCheckBox type='checkbox' id='toggleButton' onChange={handleOnChange} />
-      <ToggleLabel htmlFor='toggleButton' isChecked={isChecked} size={size} />
-      <ToggleItem isChecked={isChecked} size={size}>
-        외장
-      </ToggleItem>
-      <ToggleItem isChecked={!isChecked} size={size}>
-        내장
-      </ToggleItem>
+      <input
+        type='checkbox'
+        id='toggleButton'
+        onChange={handleOnChange}
+        css={css`
+          display: none;
+        `}
+      />
+      <ToggleLabel htmlFor='toggleButton' isChecked={isChecked} size={size}>
+        <ToggleSwitch isChecked={isChecked} size={size}></ToggleSwitch>
+        <ToggleItem isChecked={isChecked} size={size}>
+          외장
+        </ToggleItem>
+        <ToggleItem isChecked={!isChecked} size={size}>
+          내장
+        </ToggleItem>
+      </ToggleLabel>
     </ToggleContainer>
   );
 }
@@ -28,50 +38,31 @@ function Toggle({ isChecked, size, setIsChecked }: Props) {
 export default Toggle;
 
 const ToggleContainer = styled.div<Pick<Props, 'size'>>`
-  ${({ theme }) => theme.flex.flexCenterRow}
+  ${({ theme }) => theme.flex.flexCenterRow};
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray100};
+  border-radius: ${({ size }) => (size === 'small' ? 22 : 24)}px;
   width: ${({ size }) => (size === 'small' ? 154 : 213)}px;
   height: ${({ size }) => (size === 'small' ? 44 : 48)}px;
-  border-radius: ${({ size }) => (size === 'small' ? 22 : 24)}px;
-  border: 1px solid ${({ theme }) => theme.colors.gray100};
-  background-color: ${({ theme }) => theme.colors.white};
-  position: relative;
-`;
-
-const ToggleCheckBox = styled.input`
-  display: none;
+  pointer-events: none;
 `;
 
 const ToggleLabel = styled.label<Pick<Props, 'size' | 'isChecked'>>`
-  ::before {
-    position: absolute;
-    top: 3px;
-    left: 4px;
-    background: ${({ theme }) => theme.colors.white};
-    border-radius: 20px;
-    width: ${({ size }) => (size === 'small' ? 72 : 101)}px;
-    height: ${({ size }) => (size === 'small' ? 36 : 40)}px;
-    content: '';
-  }
+  ${({ theme }) => theme.flex.flexCenterRow};
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+`;
 
-  ::after {
-    position: absolute;
-    top: 3px;
-    right: 4px;
-    background-color: ${({ theme }) => theme.colors.primary500};
-    border-radius: 20px;
-    width: ${({ size }) => (size === 'small' ? 72 : 101)}px;
-    height: ${({ size }) => (size === 'small' ? 36 : 40)}px;
-    content: '';
-    pointer-events: ${({ isChecked }) => !isChecked && `none`};
-  }
-
-  &::before {
-    background-color: ${({ theme, isChecked }) => isChecked && theme.colors.primary500};
-    pointer-events: ${({ isChecked }) => isChecked && `none`};
-  }
-  &::after {
-    background-color: ${({ theme, isChecked }) => isChecked && theme.colors.white};
-  }
+const ToggleSwitch = styled.span<Pick<Props, 'size' | 'isChecked'>>`
+  position: absolute;
+  left: ${({ isChecked, size }) => (isChecked ? 0 : size === 'small' ? 72 : 101)}px;
+  background: ${({ theme }) => theme.colors.primary500};
+  border-radius: 20px;
+  width: ${({ size }) => (size === 'small' ? 72 : 101)}px;
+  height: ${({ size }) => (size === 'small' ? 36 : 40)}px;
+  transition: all 0.2s ease-in-out;
 `;
 
 const ToggleItem = styled.div<Pick<Props, 'size' | 'isChecked'>>`
@@ -81,5 +72,6 @@ const ToggleItem = styled.div<Pick<Props, 'size' | 'isChecked'>>`
   width: ${({ size }) => (size === 'small' ? 72 : 101)}px;
   height: ${({ size }) => (size === 'small' ? 36 : 40)}px;
   z-index: 1;
-  pointer-events: none;
+  transition: all 0.2s ease-in-out;
+  pointer-events: ${({ isChecked }) => (isChecked ? 'none' : 'fill')};
 `;
