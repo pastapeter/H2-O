9ee4be327b9930ba0126124016672cb1af77a300
Dohyun Chung <pastapeter@naver.com>
2023-08-09 01:38:54 +0900

    feat: APIManagerProtocol 에서 Session을 정해주기 - 미래에 나중에 다른 객체로 뺼수도 있을듯

--- iOS/Catalog/Catalog/NetworkProtocol/APIManagerProtocol.swift
+++ iOS/Catalog/Catalog/NetworkProtocol/APIManagerProtocol.swift
@@ -7,15 +7,29 @@
 
 import Foundation
 
+protocol RequestInterceptor { }
+
+protocol CachedResponseHandler { }
+
 protocol APIManagerProtocol {
   func perform(_ request: RequestProtocol) async throws -> Data
 }
 
 class APIManager: APIManagerProtocol {
-  private let urlSession: URLSession
   
-  init(urlSession: URLSession) {
+  private let urlSession: URLSession
+  private let interceptor: RequestInterceptor?
+  private let cachedResponseHandler: CachedResponseHandler?
+  
+  init(urlSession: URLSession, intercepter: RequestInterceptor? = nil, cachedResponseHandler: CachedResponseHandler? = nil) {
     self.urlSession = urlSession
+    self.cachedResponseHandler = cachedResponseHandler
+    self.interceptor = intercepter
+  }
+  
+  convenience init(configuation: URLSessionConfiguration = URLSessionConfiguration.default, interceptor: RequestInterceptor? = nil, cachedResponseHandler: CachedResponseHandler? = nil) {
+    let session = URLSession(configuration: configuation)
+    self.init(urlSession: session, intercepter: interceptor, cachedResponseHandler: cachedResponseHandler)
   }
   
   func perform(_ request: RequestProtocol) async throws -> Data {
