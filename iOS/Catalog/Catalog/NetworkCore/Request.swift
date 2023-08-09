4d2a81c9b8f48f5601f58e237d5718cc7cdcea4a
Dohyun Chung <pastapeter@naver.com>
2023-08-09 16:37:56 +0900

    feat: RetryCount Check를 위해서 urlRequest wrapping

---
+++ iOS/Catalog/Catalog/NetworkCore/Request.swift
@@ -0,0 +1,18 @@
+//
+//  Request.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/9/23.
+//
+
+import Foundation
+
+class Request {
+  var urlRequest: URLRequest
+  var retryCount: Int
+
+  init(urlRequest: URLRequest) {
+    self.urlRequest = urlRequest
+    self.retryCount = 0
+  }
+}
