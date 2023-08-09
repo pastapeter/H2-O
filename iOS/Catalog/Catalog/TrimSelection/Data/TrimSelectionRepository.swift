79e28aa7a406f656dddd21ed736796b2208136dd
Dohyun Chung <pastapeter@naver.com>
2023-08-10 00:33:57 +0900

    feat: TrimSelectionRepository 추가

---
+++ iOS/Catalog/Catalog/TrimSelection/Data/TrimSelectionRepository.swift
@@ -0,0 +1,34 @@
+//
+//  TrimSelectionRepository.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/9/23.
+//
+
+import Foundation
+
+struct Trim {
+
+}
+
+protocol TrimSelectionRepositoryProtocol {
+
+  func fetchTrims(in vehicleId: Int) async throws -> [Trim]
+
+}
+
+final class TrimSelectionRepository: TrimSelectionRepositoryProtocol {
+
+  private let trimSelectionRequestManager: RequestManagerProtocol
+
+  init(trimSelectionRequestManager: RequestManagerProtocol) {
+    self.trimSelectionRequestManager = trimSelectionRequestManager
+  }
+
+  func fetchTrims(in vehicleId: Int) async throws -> [Trim] {
+    let dto: TrimResponseDTO = try await trimSelectionRequestManager
+      .perform(TrimSelectionRequest.fetchTrimList(vehicleId: vehicleId))
+    return dto.toDomain()
+  }
+
+}
