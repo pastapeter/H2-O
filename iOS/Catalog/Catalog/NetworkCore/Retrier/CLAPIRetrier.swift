f4e9a5a5c7cb41d04ef7154a00ddc647b2045dd9
Dohyun Chung <pastapeter@naver.com>
2023-08-10 00:30:49 +0900

    feat: CLAPIRetrier 추가로 Retrier 내부적인 로직 추가

---
+++ iOS/Catalog/Catalog/NetworkCore/Retrier/CLAPIRetrier.swift
@@ -0,0 +1,36 @@
+//
+//  CLApiRetrier.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/9/23.
+//
+
+import Foundation
+
+class CLAPIRetrier: CLRequestRetrier {
+
+  let retryLimit = 3
+
+  override func retry(_ request: Request, for session: URLSessionProtocol, dueTo error: Error, completion: @escaping (CLRetryResult) -> Void) {
+
+    Task {
+      guard let response = try await session.makeData(for: request.urlRequest).response as? HTTPURLResponse else {
+        completion(.doNotRetryWithError(error))
+        return
+      }
+
+      switch response.statusCode {
+      case 13: // timeout
+        if request.retryCount < retryLimit {
+          completion(.retry)
+        } else {
+          // TODO: 타임아웃끝나면 어떻게 설정해줄거임?
+        }
+      default:
+        completion(.doNotRetry)
+      }
+
+    }
+
+  }
+}
