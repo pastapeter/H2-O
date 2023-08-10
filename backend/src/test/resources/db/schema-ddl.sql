90fdfa34fc7088e2684e1a31db63ea3c4e383dd5
Lee junho <31722326+plusjob70@users.noreply.github.com>
2023-08-11 00:31:47 +0900

    feat: 테스트를 위해 모든 테이블의 제약 조건 제거 (#111)

--- backend/src/test/resources/db/schema-ddl.sql
+++ backend/src/test/resources/db/schema-ddl.sql
@@ -95,21 +95,38 @@ CREATE TABLE `category`
     `name` VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE `trims_image`
+CREATE TABLE `powertrain_spec`
 (
-    `id`      BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `trim_id` BIGINT NOT NULL,
-    `image`   TEXT   NOT NULL,
-    FOREIGN KEY (`trim_id`) REFERENCES `trims` (`id`)
-
+    `powertrain_id`  BIGINT NOT NULL,
+    `output_ps`      FLOAT  NULL,
+    `output_rpm`     INT    NULL,
+    `torque_kgfm`    FLOAT  NULL,
+    `torque_min_rpm` INT    NULL,
+    `torque_max_rpm` INT    NULL
 );
 
-CREATE TABLE `external_color_image`
+CREATE TABLE `car_powertrain`
 (
-    `id`                BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `external_color_id` BIGINT NOT NULL,
-    `image`             TEXT   NOT NULL,
-    FOREIGN KEY (`external_color_id`) REFERENCES `external_color` (`id`)
+    `car_id`        BIGINT NOT NULL,
+    `powertrain_id` BIGINT NOT NULL,
+    `price`         INT    NOT NULL,
+    `choice_ratio`  FLOAT  NULL
+);
+
+CREATE TABLE `car_bodytype`
+(
+    `car_id`       BIGINT NOT NULL,
+    `bodytype_id`  BIGINT NOT NULL,
+    `price`        INT    NOT NULL,
+    `choice_ratio` FLOAT  NULL
+);
+
+CREATE TABLE `car_drivetrain`
+(
+    `car_id`        BIGINT NOT NULL,
+    `drivetrain_id` BIGINT NOT NULL,
+    `price`         INT    NOT NULL,
+    `choice_ratio`  FLOAT  NULL
 );
 
 CREATE TABLE `technical_spec`
@@ -120,32 +137,34 @@ CREATE TABLE `technical_spec`
     `fuel_efficiency` FLOAT  NOT NULL
 );
 
-CREATE TABLE `car_powertrain`
+CREATE TABLE `external_color_image`
 (
-    `car_id`        BIGINT NOT NULL,
-    `powertrain_id` BIGINT NOT NULL,
-    `price`         INT    NOT NULL
+    `id`                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `external_color_id` BIGINT NOT NULL,
+    `image`             TEXT   NOT NULL
 );
 
-CREATE TABLE `car_bodytype`
+CREATE TABLE `trims_image`
 (
-    `car_id`      BIGINT NOT NULL,
-    `bodytype_id` BIGINT NOT NULL,
-    `price`       INT    NOT NULL
+    `id`      BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `trim_id` BIGINT NOT NULL,
+    `image`   TEXT   NOT NULL
 );
 
-CREATE TABLE `car_drivetrain`
+CREATE TABLE `trims_external_color`
 (
-    `car_id`        BIGINT NOT NULL,
-    `drivetrain_id` BIGINT NOT NULL,
-    `price`         INT    NOT NULL
+    `trim_id`           BIGINT NOT NULL,
+    `external_color_id` BIGINT NOT NULL,
+    `price`             INT    NOT NULL,
+    `choice_ratio`      FLOAT  NULL
 );
 
 CREATE TABLE `trims_internal_color`
 (
     `trim_id`           BIGINT NOT NULL,
     `internal_color_id` BIGINT NOT NULL,
-    `price`             INT    NOT NULL
+    `price`             INT    NOT NULL,
+    `choice_ratio`      FLOAT  NULL
 );
 
 CREATE TABLE `trims_package`
@@ -165,6 +184,24 @@ CREATE TABLE `trims_options`
     `choice_ratio` FLOAT       NULL
 );
 
+CREATE TABLE `options_hashtag`
+(
+    `option_id`  BIGINT NOT NULL,
+    `hashtag_id` BIGINT NOT NULL
+);
+
+CREATE TABLE `options_quotation`
+(
+    `option_id`    BIGINT NOT NULL,
+    `quotation_id` BIGINT NOT NULL
+);
+
+CREATE TABLE `options_category`
+(
+    `option_id`   BIGINT NOT NULL,
+    `category_id` BIGINT NOT NULL
+);
+
 CREATE TABLE `package_options`
 (
     `package_id` BIGINT NOT NULL,
@@ -175,25 +212,6 @@ CREATE TABLE `package_hashtag`
 (
     `package_id` BIGINT NOT NULL,
     `hashtag_id` BIGINT NOT NULL
-);
-
-CREATE TABLE `options_hashtag`
-(
-    `option_id`  BIGINT NOT NULL,
-    `hashtag_id` BIGINT NOT NULL
-);
-
-CREATE TABLE `trims_external_color`
-(
-    `trim_id`           BIGINT NOT NULL,
-    `external_color_id` BIGINT NOT NULL,
-    `price`             INT    NOT NULL
-);
-
-CREATE TABLE `options_quotation`
-(
-    `option_id`    BIGINT NOT NULL,
-    `quotation_id` BIGINT NOT NULL
 );
 
 CREATE TABLE `package_quotation`
@@ -207,238 +225,3 @@ CREATE TABLE `package_category`
     `package_id`  BIGINT NOT NULL,
     `category_id` BIGINT NOT NULL
 );
-
-CREATE TABLE `options_category`
-(
-    `option_id`   BIGINT NOT NULL,
-    `category_id` BIGINT NOT NULL
-);
-
-CREATE TABLE `powertrain_spec`
-(
-    `powertrain_id`  BIGINT NOT NULL,
-    `output_ps`      FLOAT  NULL,
-    `output_rpm`     INT    NULL,
-    `torque_kgfm`    FLOAT  NULL,
-    `torque_min_rpm` INT    NULL,
-    `torque_max_rpm` INT    NULL
-);
-
-ALTER TABLE `technical_spec`
-    ADD CONSTRAINT `PK_TECHNICAL_SPEC`
-        PRIMARY KEY (`powertrain_id`, `drivetrain_id`);
-
-ALTER TABLE `car_powertrain`
-    ADD CONSTRAINT `PK_CAR_POWERTRAIN`
-        PRIMARY KEY (`car_id`, `powertrain_id`);
-
-ALTER TABLE `car_bodytype`
-    ADD CONSTRAINT `PK_CAR_BODYTYPE`
-        PRIMARY KEY (`car_id`, `bodytype_id`);
-
-ALTER TABLE `car_drivetrain`
-    ADD CONSTRAINT `PK_CAR_DRIVETRAIN`
-        PRIMARY KEY (`car_id`, `drivetrain_id`);
-
-ALTER TABLE `trims_internal_color`
-    ADD CONSTRAINT `PK_TRIMS_INTERNAL_COLOR`
-        PRIMARY KEY (`trim_id`, `internal_color_id`);
-
-ALTER TABLE `trims_package`
-    ADD CONSTRAINT `PK_TRIMS_PACKAGE`
-        PRIMARY KEY (`trim_id`, `package_id`);
-
-ALTER TABLE `trims_options`
-    ADD CONSTRAINT `PK_TRIMS_OPTIONS`
-        PRIMARY KEY (`trim_id`, `option_id`);
-
-ALTER TABLE `package_options`
-    ADD CONSTRAINT `PK_PACKAGE_OPTIONS`
-        PRIMARY KEY (`package_id`, `option_id`);
-
-ALTER TABLE `package_hashtag`
-    ADD CONSTRAINT `PK_PACKAGE_HASHTAG`
-        PRIMARY KEY (`package_id`, `hashtag_id`);
-
-ALTER TABLE `options_hashtag`
-    ADD CONSTRAINT `PK_OPTIONS_HASHTAG`
-        PRIMARY KEY (`option_id`, `hashtag_id`);
-
-ALTER TABLE `trims_external_color`
-    ADD CONSTRAINT `PK_TRIMS_EXTERNAL_COLOR`
-        PRIMARY KEY (`trim_id`, `external_color_id`);
-
-ALTER TABLE `options_quotation`
-    ADD CONSTRAINT `PK_OPTIONS_QUOTATION`
-        PRIMARY KEY (`option_id`, `quotation_id`);
-
-ALTER TABLE `package_quotation`
-    ADD CONSTRAINT `PK_PACKAGE_QUOTATION`
-        PRIMARY KEY (`package_id`, `quotation_id`);
-
-ALTER TABLE `package_category`
-    ADD CONSTRAINT `PK_PACKAGE_CATEGORY`
-        PRIMARY KEY (`package_id`, `category_id`);
-
-ALTER TABLE `options_category`
-    ADD CONSTRAINT `PK_OPTIONS_CATEGORY`
-        PRIMARY KEY (`option_id`, `category_id`);
-
-ALTER TABLE `powertrain_spec`
-    ADD CONSTRAINT `PK_POWERTRAIN_SPEC`
-        PRIMARY KEY (`powertrain_id`);
-
-ALTER TABLE `technical_spec`
-    ADD CONSTRAINT `FK_powertrain_TO_technical_spec_1`
-        FOREIGN KEY (`powertrain_id`)
-            REFERENCES `powertrain` (`id`);
-
-ALTER TABLE `technical_spec`
-    ADD CONSTRAINT `FK_drivetrain_TO_technical_spec_1`
-        FOREIGN KEY (`drivetrain_id`)
-            REFERENCES `drivetrain` (`id`);
-
-ALTER TABLE `car_powertrain`
-    ADD CONSTRAINT `FK_car_TO_car_powertrain_1`
-        FOREIGN KEY (`car_id`)
-            REFERENCES `car` (`id`);
-
-ALTER TABLE `car_powertrain`
-    ADD CONSTRAINT `FK_powertrain_TO_car_powertrain_1`
-        FOREIGN KEY (`powertrain_id`)
-            REFERENCES `powertrain` (`id`);
-
-ALTER TABLE `car_bodytype`
-    ADD CONSTRAINT `FK_car_TO_car_bodytype_1`
-        FOREIGN KEY (`car_id`)
-            REFERENCES `car` (`id`);
-
-ALTER TABLE `car_bodytype`
-    ADD CONSTRAINT `FK_bodytype_TO_car_bodytype_1`
-        FOREIGN KEY (`bodytype_id`)
-            REFERENCES `bodytype` (`id`);
-
-ALTER TABLE `car_drivetrain`
-    ADD CONSTRAINT `FK_car_TO_car_drivetrain_1`
-        FOREIGN KEY (`car_id`)
-            REFERENCES `car` (`id`);
-
-ALTER TABLE `car_drivetrain`
-    ADD CONSTRAINT `FK_drivetrain_TO_car_drivetrain_1`
-        FOREIGN KEY (`drivetrain_id`)
-            REFERENCES `drivetrain` (`id`);
-
-ALTER TABLE `trims_internal_color`
-    ADD CONSTRAINT `FK_trims_TO_trims_internal_color_1`
-        FOREIGN KEY (`trim_id`)
-            REFERENCES `trims` (`id`);
-
-ALTER TABLE `trims_internal_color`
-    ADD CONSTRAINT `FK_internal_color_TO_trims_internal_color_1`
-        FOREIGN KEY (`internal_color_id`)
-            REFERENCES `internal_color` (`id`);
-
-ALTER TABLE `trims_package`
-    ADD CONSTRAINT `FK_trims_TO_trims_package_1`
-        FOREIGN KEY (`trim_id`)
-            REFERENCES `trims` (`id`);
-
-ALTER TABLE `trims_package`
-    ADD CONSTRAINT `FK_package_TO_trims_package_1`
-        FOREIGN KEY (`package_id`)
-            REFERENCES `package` (`id`);
-
-ALTER TABLE `trims_options`
-    ADD CONSTRAINT `FK_trims_TO_trims_options_1`
-        FOREIGN KEY (`trim_id`)
-            REFERENCES `trims` (`id`);
-
-ALTER TABLE `trims_options`
-    ADD CONSTRAINT `FK_options_TO_trims_options_1`
-        FOREIGN KEY (`option_id`)
-            REFERENCES `options` (`id`);
-
-ALTER TABLE `package_options`
-    ADD CONSTRAINT `FK_package_TO_package_options_1`
-        FOREIGN KEY (`package_id`)
-            REFERENCES `package` (`id`);
-
-ALTER TABLE `package_options`
-    ADD CONSTRAINT `FK_options_TO_package_options_1`
-        FOREIGN KEY (`option_id`)
-            REFERENCES `options` (`id`);
-
-ALTER TABLE `package_hashtag`
-    ADD CONSTRAINT `FK_package_TO_package_hashtag_1`
-        FOREIGN KEY (`package_id`)
-            REFERENCES `package` (`id`);
-
-ALTER TABLE `package_hashtag`
-    ADD CONSTRAINT `FK_hashtag_TO_package_hashtag_1`
-        FOREIGN KEY (`hashtag_id`)
-            REFERENCES `hashtag` (`id`);
-
-ALTER TABLE `options_hashtag`
-    ADD CONSTRAINT `FK_options_TO_options_hashtag_1`
-        FOREIGN KEY (`option_id`)
-            REFERENCES `options` (`id`);
-
-ALTER TABLE `options_hashtag`
-    ADD CONSTRAINT `FK_hashtag_TO_options_hashtag_1`
-        FOREIGN KEY (`hashtag_id`)
-            REFERENCES `hashtag` (`id`);
-
-ALTER TABLE `trims_external_color`
-    ADD CONSTRAINT `FK_trims_TO_trims_external_color_1`
-        FOREIGN KEY (`trim_id`)
-            REFERENCES `trims` (`id`);
-
-ALTER TABLE `trims_external_color`
-    ADD CONSTRAINT `FK_external_color_TO_trims_external_color_1`
-        FOREIGN KEY (`external_color_id`)
-            REFERENCES `external_color` (`id`);
-
-ALTER TABLE `options_quotation`
-    ADD CONSTRAINT `FK_options_TO_options_quotation_1`
-        FOREIGN KEY (`option_id`)
-            REFERENCES `options` (`id`);
-
-ALTER TABLE `options_quotation`
-    ADD CONSTRAINT `FK_quotation_TO_options_quotation_1`
-        FOREIGN KEY (`quotation_id`)
-            REFERENCES `quotation` (`id`);
-
-ALTER TABLE `package_quotation`
-    ADD CONSTRAINT `FK_package_TO_package_quotation_1`
-        FOREIGN KEY (`package_id`)
-            REFERENCES `package` (`id`);
-
-ALTER TABLE `package_quotation`
-    ADD CONSTRAINT `FK_quotation_TO_package_quotation_1`
-        FOREIGN KEY (`quotation_id`)
-            REFERENCES `quotation` (`id`);
-
-ALTER TABLE `package_category`
-    ADD CONSTRAINT `FK_package_TO_package_category_1`
-        FOREIGN KEY (`package_id`)
-            REFERENCES `package` (`id`);
-
-ALTER TABLE `package_category`
-    ADD CONSTRAINT `FK_category_TO_package_category_1`
-        FOREIGN KEY (`category_id`)
-            REFERENCES `category` (`id`);
-
-ALTER TABLE `options_category`
-    ADD CONSTRAINT `FK_options_TO_options_category_1`
-        FOREIGN KEY (`option_id`)
-            REFERENCES `options` (`id`);
-
-ALTER TABLE `options_category`
-    ADD CONSTRAINT `FK_category_TO_options_category_1`
-        FOREIGN KEY (`category_id`)
-            REFERENCES `category` (`id`);
-
-ALTER TABLE `powertrain_spec`
-    ADD CONSTRAINT `FK_powertrain_TO_powertrain_spec_1`
-        FOREIGN KEY (`powertrain_id`)
-            REFERENCES `powertrain` (`id`);
