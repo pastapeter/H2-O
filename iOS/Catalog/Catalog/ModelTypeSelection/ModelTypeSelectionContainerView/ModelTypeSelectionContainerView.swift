2cf55027a48e76e2c7b41a661a5ae34c33f8c0f2
Dohyun Chung <pastapeter@naver.com>
2023-08-13 01:07:58 +0900

    chore: BlurredFullScreenCover 관련 Code, Component로 옮기기

--- iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeSelectionContainerView/ModelTypeSelectionContainerView.swift
+++ iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeSelectionContainerView/ModelTypeSelectionContainerView.swift
@@ -48,53 +48,7 @@ struct ModelTypeSelectionContainerView: View {
 }
 
 struct ModelTypeSelectionContainerView_Previews: PreviewProvider {
-    static var previews: some View {
-        ModelTypeSelectionContainerView()
-    }
-}
-
-extension View {
-
-  func blurredSheet<Content: View>(_ style: AnyShapeStyle, show: Binding<Bool>, onDismiss: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) -> some View {
-    self
-      .sheet(isPresented: show, onDismiss: onDismiss) {
-        content()
-          .background(RemovebackgroundColor())
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
-          .background {
-            Rectangle()
-              .fill(style)
-              .ignoresSafeArea(.container, edges: .all)
-          }
-      }
+  static var previews: some View {
+    ModelTypeSelectionContainerView()
   }
-
-  func blurredFullScreenCover<Content: View>(_ style: AnyShapeStyle, show: Binding<Bool>, onDismiss: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) -> some View {
-    self
-      .fullScreenCover(isPresented: show, onDismiss: onDismiss) {
-        content()
-          .background(RemovebackgroundColor())
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
-          .background {
-            Rectangle()
-              .fill(style)
-              .ignoresSafeArea(.container, edges: .all)
-          }
-      }
-  }
-
-}
-
-private struct RemovebackgroundColor: UIViewRepresentable {
-
-  func makeUIView(context: Context) -> some UIView {
-    return UIView()
-  }
-
-  func updateUIView(_ uiView: UIViewType, context: Context) {
-    DispatchQueue.main.async {
-      uiView.superview?.superview?.backgroundColor = .clear
-    }
-  }
-
 }
