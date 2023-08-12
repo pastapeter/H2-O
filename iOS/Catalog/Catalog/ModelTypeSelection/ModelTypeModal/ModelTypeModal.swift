f17d40e96fb170aa4fcda502cb7a0a52668ffb1f
Dohyun Chung <pastapeter@naver.com>
2023-08-12 12:18:38 +0900

    feat: ModelTypeModal 추가로 Modal 올리기

---
+++ iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeModal/ModelTypeModal.swift
@@ -0,0 +1,20 @@
+//
+//  ModelTypeModal.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/11/23.
+//
+
+import SwiftUI
+
+struct ModelTypeModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelTypeModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelTypeModal()
+    }
+}
