ecdbd0986f338e33db892d8bc7b57c5062e5a670
Dohyun Chung <pastapeter@naver.com>
2023-08-14 01:39:21 +0900

    chore: 필요없는 geometry Reader 제거

--- iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeModal/ModelContentView.swift
+++ iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeModal/ModelContentView.swift
@@ -24,36 +24,34 @@ extension ModelContentView {
 
   var body: some View {
     VStack(alignment: .leading) {
-      GeometryReader { _ in
-        VStack {
-          Image("gasoline3")
-            .resizable()
-            .frame(height: UIScreen.main.bounds.height * (180 / 812))
-          VStack(alignment: .leading) {
-            HStack {
-              Text(content.title)
-                .catalogFont(type: .HeadKRMedium18)
-                .foregroundColor(.gray900)
-              Spacer()
-              Text("\(Text("\(content.frequency)%").foregroundColor(.activeBlue))의 선택")
-                .catalogFont(type: .TextKRMedium12)
-            }
-            Text(content.description)
-              .catalogFont(type: .TextKRRegular12)
-              .foregroundColor(.gray800)
-              .multilineTextAlignment(.leading)
+      VStack {
+        Image("gasoline3")
+          .resizable()
+          .frame(height: UIScreen.main.bounds.height * (180 / 812))
+        VStack(alignment: .leading) {
+          HStack {
+            Text(content.title)
+              .catalogFont(type: .HeadKRMedium18)
+              .foregroundColor(.gray900)
+            Spacer()
+            Text("\(Text("\(content.frequency)%").foregroundColor(.activeBlue))의 선택")
+              .catalogFont(type: .TextKRMedium12)
           }
-          .padding(.top, 12)
-          .padding(.horizontal, 20)
-          Spacer()
-          if let hmgDataState = state.hmgData {
-            HMGDataBannerComponent {
-              HMGDataWithHorizontalGraphView(state: hmgDataState)
-            }
+          Text(content.description)
+            .catalogFont(type: .TextKRRegular12)
+            .foregroundColor(.gray800)
+            .multilineTextAlignment(.leading)
+        }
+        .padding(.top, 12)
+        .padding(.horizontal, 20)
+        Spacer()
+        if let hmgDataState = state.hmgData {
+          HMGDataBannerComponent {
+            HMGDataWithHorizontalGraphView(state: hmgDataState)
           }
         }
-        .background(.white)
       }
+      .background(.white)
     }
   }
 
