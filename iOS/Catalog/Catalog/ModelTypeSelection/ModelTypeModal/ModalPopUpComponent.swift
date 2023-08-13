557be9f695d2e36cf416e11cee7b8d730956ea61
Dohyun Chung <pastapeter@naver.com>
2023-08-14 01:37:15 +0900

    chore: coding convention

--- iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeModal/ModalPopUpComponent.swift
+++ iOS/Catalog/Catalog/ModelTypeSelection/ModelTypeModal/ModalPopUpComponent.swift
@@ -8,44 +8,56 @@
 import SwiftUI
 
 struct ModalPopUpComponent<ModalPopUpContent: View>: View {
-
+  
   var state: ModelTypeContent
+  
   var submitAction: () -> Void
+  
   @Environment(\.dismiss) private var dismiss
+  
   @State private var animateView: Bool = false
+  
   @State private var animateContent: Bool = false
+  
   @ViewBuilder var content: () -> ModalPopUpContent
+  
+}
 
-    var body: some View {
-      VStack {
-        if animateView {
-          VStack {
-            titleView(state.title)
-                .padding(.horizontal, 16)
-              content()
-            CLButton(subText: state.price.signedWon, mainText: "선택하기", height: 87, backgroundColor: .activeBlue, buttonAction: submitAction)
-                .frame(height: 56)
-          }
-          .background(.white)
-          .cornerRadius(4)
-          .frame(height: UIScreen.main.bounds.height * 0.67)
-          .padding(.horizontal, 30)
+extension ModalPopUpComponent {
+  
+  var body: some View {
+    VStack {
+      if animateView {
+        VStack {
+          titleView(state.title)
+            .padding(.horizontal, 16)
+          content()
+          CLButton(subText: state.price.signedWon, mainText: "선택하기", height: 87, backgroundColor: .activeBlue, buttonAction: submitAction)
+            .frame(height: 56)
         }
-      }
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .background(content: {
-        DimmedZStack { }
-          .edgesIgnoringSafeArea(.all)
-          .opacity(animateContent ? 0.8 : 0)
-      })
-      .onAppear {
-        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.8, blendDuration: 0.8)) {
-          animateView = true
-          animateContent = true
-        }
+        .background(.white)
+        .cornerRadius(4)
+        .frame(height: UIScreen.main.bounds.height * 0.67)
+        .padding(.horizontal, 30)
       }
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(content: {
+      DimmedZStack { }
+        .edgesIgnoringSafeArea(.all)
+        .opacity(animateContent ? 0.8 : 0)
+    })
+    .onAppear {
+      withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.8, blendDuration: 0.8)) {
+        animateView = true
+        animateContent = true
+      }
+    }
+  }
+  
+}
 
+extension ModalPopUpComponent {
   @ViewBuilder
   func titleView(_ text: String) -> some View {
     ZStack(alignment: .center) {
@@ -71,9 +83,9 @@ struct ModalPopUpComponent<ModalPopUpContent: View>: View {
 }
 
 struct ModalPopUpComponent_Previews: PreviewProvider {
-    static var previews: some View {
-      ModalPopUpComponent(state: .mock(), submitAction: { }) {
-        ModelContentView(state: .init(content: .mock(), hmgData: .mock()))
-      }
+  static var previews: some View {
+    ModalPopUpComponent(state: .mock(), submitAction: { }) {
+      ModelContentView(state: .init(content: .mock(), hmgData: .mock()))
     }
+  }
 }
