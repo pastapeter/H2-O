de6cc61cc07b014677986adbb7e13cc028b28fca
Dohyun Chung <pastapeter@naver.com>
2023-08-15 02:48:14 +0900

    feat: Hex Code 기반으로 Color 기능 추가

--- iOS/Catalog/Catalog/Colors/Color+.swift
+++ iOS/Catalog/Catalog/Colors/Color+.swift
@@ -28,8 +28,8 @@ extension Color {
   static let gray800 = Color("Gray800")
   static let gray900 = Color("Gray900")
 
-    static let primary0 = Color("PrimaryColor0")
-   static let primary100 = Color("PrimaryColor100")
+  static let primary0 = Color("PrimaryColor0")
+  static let primary100 = Color("PrimaryColor100")
   static let primary200 = Color("PrimaryColor200")
   static let primary300 = Color("PrimaryColor300")
   static let primary400 = Color("PrimaryColor400")
@@ -42,4 +42,21 @@ extension Color {
   static let sand = Color("Sand")
   static let sand2 = Color("Sand2")
   static let lightSand = Color("LightSand")
+}
+
+extension Color {
+
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+
 }
