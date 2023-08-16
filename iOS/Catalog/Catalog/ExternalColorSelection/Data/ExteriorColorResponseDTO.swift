5051baf239290034c2afbe675196ecce735ff7d8
Dohyun Chung <pastapeter@naver.com>
2023-08-16 11:50:10 +0900

    feat: ExteriorColorResponse -> toDomain

--- iOS/Catalog/Catalog/ExternalColorSelection/Data/ExteriorColorResponseDTO.swift
+++ iOS/Catalog/Catalog/ExternalColorSelection/Data/ExteriorColorResponseDTO.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+enum ExteriorColorToDomainError: LocalizedError {
+  case noIdInResponse
+  case noNameInResponse
+  case noChoiceRatioInResponse
+  case noPriceInResponse
+  case noHexCodeInResponse
+}
+
 struct ExteriorColorResponseDTO: Decodable {
   let id: Int?
   let name: String?
@@ -17,7 +25,31 @@ struct ExteriorColorResponseDTO: Decodable {
 }
 
 extension ExteriorColorResponseDTO {
-  
-  func toDomain() throws -> ExteriorColor
-  
+
+  func toDomain() throws -> ExteriorColor {
+
+    guard let id = id else { throw ExteriorColorToDomainError.noIdInResponse }
+    guard let name = name else { throw ExteriorColorToDomainError.noNameInResponse }
+    guard let price = price else { throw ExteriorColorToDomainError.noPriceInResponse }
+    guard let hexCode = hexCode else { throw ExteriorColorToDomainError.noHexCodeInResponse }
+
+    var domainChoiceRatio: CLNumber?
+    if let choiceRatio = choiceRatio {
+      domainChoiceRatio = CLNumber(Int32(choiceRatio * 100))
+    }
+
+    var exteriorImages: [URL] = []
+    if let images = images {
+      exteriorImages = images.compactMap { URL(string: $0) }
+    }
+
+    return ExteriorColor(id: id,
+                         name: name,
+                         choiceRatio: domainChoiceRatio,
+                         price: CLNumber(Int32(price)),
+                         hexCode: hexCode,
+                         exteriorImages: exteriorImages)
+
+  }
+
 }
