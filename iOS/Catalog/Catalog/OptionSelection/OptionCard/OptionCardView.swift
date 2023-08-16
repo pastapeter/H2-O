cfc7dc5a349c8eb82cc5c664b8ce975bef66c772
Dohyun Chung <pastapeter@naver.com>
2023-08-16 18:26:08 +0900

    feat: OptionCardView에 ImageTag, HMGButton 추가

--- iOS/Catalog/Catalog/OptionSelection/OptionCard/OptionCardView.swift
+++ iOS/Catalog/Catalog/OptionSelection/OptionCard/OptionCardView.swift
@@ -10,13 +10,31 @@ import SwiftUI
 struct OptionCardView: View {
 
   var choiceRatioExist: Bool = true
-  var isSelected: Bool = false
+  var isSelected: Bool = true
   var name: String = "컴포트2"
   var price = CLNumber(1090000)
 
     var body: some View {
       VStack(spacing: 0) {
-        Rectangle().fill(.blue)
+        ZStack {
+          Image("cooling")
+            .resizable()
+          VStack(spacing: 0) {
+            HStack {
+              Spacer()
+              HMGButton(action: { })
+            }
+            Spacer()
+            HStack {
+              ImageTagView(title: "캠핑")
+              ImageTagView(title: "장거리운행")
+              ImageTagView(title: "겨울운전")
+              Spacer()
+            }
+          }
+          .padding(.leading, 13)
+          .padding(.bottom, 8)
+        }
         VStack(alignment: .leading, spacing: 0) {
           Spacer().frame(height: 12)
           if choiceRatioExist {
@@ -44,12 +62,12 @@ struct OptionCardView: View {
               Rectangle()
                 .foregroundColor(.clear)
                 .frame(width: 40, height: 40)
-                .background(Color.gray50)
+                .background(isSelected ? .activeBlue : Color.gray50)
                 .overlay(
                   RoundedRectangle(cornerRadius: 2)
                     .inset(by: 0.5)
-                    .stroke(Color.gray100, lineWidth: 1))
-              Image("check").renderingMode(.template).foregroundColor(isSelected ? .activeBlue2 : .gray200)
+                    .stroke(isSelected ? .activeBlue: Color.gray100, lineWidth: 1))
+              Image("check").renderingMode(.template).foregroundColor(isSelected ? .white : .gray200)
             }
           }
           Spacer().frame(height: 12)
@@ -57,12 +75,35 @@ struct OptionCardView: View {
         .padding(.horizontal, 12)
       }
       .frame(height: UIScreen.main.bounds.height * 212 / 812)
-      .background(Color.background2)
+      .optionCardBackground(isSelected: isSelected)
+      .cornerRadius(2)
     }
+
 }
 
 struct OptionCardView_Previews: PreviewProvider {
     static var previews: some View {
         OptionCardView()
     }
+}
+
+fileprivate extension View {
+
+  @ViewBuilder
+  func optionCardBackground(isSelected: Bool) -> some View {
+    if isSelected {
+      modifier(ModelButtonSelectedBackground())
+    } else {
+      modifier(ModelButtonUnSelectedBackground())
+    }
+  }
+
+}
+
+private struct OptionCardUnSelectedBackground: ViewModifier {
+
+  func body(content: Content) -> some View {
+    content
+      .background(Color.background2)
+  }
 }
