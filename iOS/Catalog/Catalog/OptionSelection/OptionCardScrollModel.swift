f5c0de89744e01c0bfee6c3c1155b386d8649951
Dohyun Chung <pastapeter@naver.com>
2023-08-17 16:41:52 +0900

    feat: FilterState 추가하여서, OptionCardScrollModel에서 내려보내주기

--- iOS/Catalog/Catalog/OptionSelection/OptionCardScrollModel.swift
+++ iOS/Catalog/Catalog/OptionSelection/OptionCardScrollModel.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 enum OptionCardScrollModel {
-  
+
   struct State {
-    var filters = OptionFilter.additionalOptionFilter
+    var filterState: FilterState = .init()
     var cardStates: [OptionCardModel.State]
-    var selectedFilterId: Int = 0
+    var selectedOptionId: Int = 0
     var startIndex: Int = 0
     var endIndex: Int = 0
   }
-  
+
   enum ViewAction {
     case onAppear
     case onTapFilterButton(index: Int)
