236be36f94b4abc1b664f77d51751ebfee7a51e1
Dohyun Chung <pastapeter@naver.com>
2023-08-19 03:47:32 +0900

    fix: conflict 해결

--- iOS/Catalog/Catalog/OptionSelection/Entity/ExtraOption.swift
+++ iOS/Catalog/Catalog/OptionSelection/Entity/ExtraOption.swift
@@ -13,16 +13,9 @@ struct ExtraOption {
   var isPackage: Bool
   var category: OptionCategory
   var name: String
-<<<<<<< HEAD:iOS/Catalog/Catalog/OptionSelection/Domain/ExtraOptionModel.swift
-  var image: URL?
-  var hashTags: [HashTag]
-  var conainsHmgData: Bool
-  var choiceRatio: Int
-=======
   var hashTags: [String]
   var containsHmgData: Bool
   var choiceRatio: CLNumber?
->>>>>>> 86e46e6 (feat: DefaultOption, ExttaOption entity 추가):iOS/Catalog/Catalog/OptionSelection/Data/Entity/ExtraOption.swift
   var price: CLNumber
   var image: URL?
   
