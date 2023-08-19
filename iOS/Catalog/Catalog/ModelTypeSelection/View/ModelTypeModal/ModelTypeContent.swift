eaa0937d59b8ab91e5d54112fba4bdeb485433fd
Dohyun Chung <pastapeter@naver.com>
2023-08-19 19:41:57 +0900

    fix: ModelTypeContent 구조체 변경으로 인한 View 변경

--- iOS/Catalog/Catalog/ModelTypeSelection/View/ModelTypeModal/ModelTypeContent.swift
+++ iOS/Catalog/Catalog/ModelTypeSelection/View/ModelTypeModal/ModelTypeContent.swift
@@ -12,15 +12,15 @@ struct ModelTypeContent: Equatable {
   static func mock() -> Self {
     return ModelTypeContent(title: "디젤 2.2",
                             description: "높은 토크로 파워풀한 드라이빙이 가능하며, 차급대비 연비 효율이 우수합니다.",
-                            frequency: 38,
+                            choiceRatio: CLNumber(38),
                             imageURL: nil,
                             price: .init(2800000)
     )
   }
 
   var title: String
-  var description: String
-  var frequency: Int
+  var description: String?
+  var choiceRatio: CLNumber?
   var imageURL: URL?
   var price: CLNumber
 }
