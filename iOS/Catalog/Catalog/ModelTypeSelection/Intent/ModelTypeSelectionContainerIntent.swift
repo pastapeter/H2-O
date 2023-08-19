38c2c565553f04cd6351f53a44973232d426c939
Dohyun Chung <pastapeter@naver.com>
2023-08-19 18:49:40 +0900

    feat: ModelTypeIntent 데이터 변환 작업

--- iOS/Catalog/Catalog/ModelTypeSelection/Intent/ModelTypeSelectionContainerIntent.swift
+++ iOS/Catalog/Catalog/ModelTypeSelection/Intent/ModelTypeSelectionContainerIntent.swift
@@ -34,6 +34,7 @@ final class ModelTypeSelectionContainerIntent: ObservableObject {
   @Published var state: State
 
   var cancellable: Set<AnyCancellable> = []
+  
 }
 
 extension ModelTypeSelectionContainerIntent: ModelTypeSelectionContainerIntentType, IntentType {
@@ -48,12 +49,56 @@ extension ModelTypeSelectionContainerIntent: ModelTypeSelectionContainerIntentTy
     case .calculateFuelEfficiency:
       return
     case .modelTypeOptions(let options):
-      state.modelTypeStateArray = options.map({
-        ModelTypeModel.State.init(title: $0.title,
-                                  optionStates: $0.options,
-                                  modelTypeDetailState: [.init(content: .mock(), hmgData: .mock()), .init(content: .mock(), hmgData: .mock())])
-      })
+      state.modelTypeStateArray = convertToModelTypeModelState(from: options)
     }
   }
 
+}
+
+// MARK: - Private Function
+
+extension ModelTypeSelectionContainerIntent {
+
+  private func convertToModelTypeModelState(from options: [ModelType]) -> [ModelTypeModel.State] {
+    options.map {
+      .init(title: $0.title,
+            imageURL: $0.options[0].imageURL,
+            containsHMGData: $0.options[0].maxOuputFromEngine != nil,
+            optionStates: convertToModelTypeOptionState(from: $0.options),
+            modelTypeDetailState: convertToModelTypeDetail(from: $0.options)
+      )
+    }
+     
+  }
+  
+  private func convertToModelTypeOptionState(from options: [ModelTypeOption]) -> [ModelTypeOptionState] {
+    
+    var result = options.map {
+      ModelTypeOptionState.init(id: $0.id, isSelected: false, choiceRatio: $0.choiceRatio, title: $0.name, price: $0.price)
+    }
+    
+    if result.count > 0 {
+      result[0].isSelected = true
+    }
+    
+    return result
+  }
+  
+  private func convertToModelTypeDetail(from options: [ModelTypeOption]) -> [ModelTypeDetailState] {
+  
+    let content = options.map {
+      ModelTypeContent.init(title: $0.name, description: $0.description, choiceRatio: $0.choiceRatio, imageURL: $0.imageURL, price: $0.price)
+    }
+    
+    let hmgData: [HMGModelTypeState?] = options.map {
+      guard let maxOutput = $0.maxOuputFromEngine, let maxTorque = $0.maxTorqueFromEngine else { return nil }
+      return HMGModelTypeState(EngineOutput: maxOutput, Torque: maxTorque)
+    }
+    
+    return zip(content, hmgData).map {
+      ModelTypeDetailState(content: $0.0, hmgData: $0.1)
+    }
+    
+  }
+  
 }
