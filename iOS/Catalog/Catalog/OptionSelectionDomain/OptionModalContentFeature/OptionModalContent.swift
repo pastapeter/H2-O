d4355155eb7496b5504efebea8c479fb23a0928a
Dohyun Chung <pastapeter@naver.com>
2023-08-21 12:32:15 +0900

    feat: ModalItemable 채택을 통해서 CLDialog 로 띄우기

---
+++ iOS/Catalog/Catalog/OptionSelectionDomain/OptionModalContentFeature/OptionModalContent.swift
@@ -0,0 +1,16 @@
+//
+//  OptionModalContentState.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/21/23.
+//
+
+import Foundation
+
+struct OptionModalContent: ModalItemable {
+  
+  var id: Int
+  var price: CLNumber
+  var title: String
+  
+}
