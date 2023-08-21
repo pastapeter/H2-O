596c35b1f9bf91303931649207fd371bb4f97e89
Dohyun Chung <pastapeter@naver.com>
2023-08-22 03:33:21 +0900

    feat: OptionModalImageView 추가하기

--- iOS/Catalog/Catalog/OptionSelectionDomain/Components/OptionModalImageView.swift
+++ iOS/Catalog/Catalog/OptionSelectionDomain/Components/OptionModalImageView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct OptionModalImageView: View {
   
-  var hashTags = ["장거리운전", "다자녀", "안전"]
+  var imageURL: URL?
+  var hashTags: [String] = []
   
     var body: some View {
       ZStack {
-        Image("cooling")
-          .resizable()
+        AsyncImage(url: imageURL) { image in
+          image
+            .resizable()
+            .scaledToFill()
+            .frame(height: CGFloat(170).scaledHeight)
+        } placeholder: {
+          ProgressView()
+        }
           
         VStack(spacing: 0) {
           HStack {
