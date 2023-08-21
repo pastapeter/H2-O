0bd95309dc376bec846c6e4e3e33abd4c371d446
Dohyun Chung <pastapeter@naver.com>
2023-08-22 03:30:52 +0900

    feat: ModalTabContentView 데이터 바인딩

--- iOS/Catalog/Catalog/OptionSelectionDomain/OptionModalContentFeature/OptionModalTabContentView.swift
+++ iOS/Catalog/Catalog/OptionSelectionDomain/OptionModalContentFeature/OptionModalTabContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct OptionModalTabContentView: View {
   
   @State var currentPage: Int = 0
+  var state: PackageInfo
   
     var body: some View {
       VStack(alignment: .leading) {
         CLNavigationMenuView(currentPage: $currentPage,
-                             navigationMenuTitles: ["후석 승객 알림", "메탈 리어범퍼스탭", "메탈 도어스커"],
+                             navigationMenuTitles: state.components.map { $0.name },
                              titleFont: .TextKRMedium14,
                              verticalSpacing:0,
                              barType: .dynamic,
@@ -23,11 +24,20 @@ struct OptionModalTabContentView: View {
           .padding(.leading, CGFloat(30).scaledWidth)
         
         TabView(selection: $currentPage) {
-          OptionModalContentView(hashTags: ["장거리 운전", "다자녀", "안전"], description: "시동이 걸린 상태에서 해당 좌석의 통풍 스위치를 누르면 표시등이 켜지면서 해당 좌석에 바람이 나오는 편의장치입니다. 시동이 걸린 상태에서 해당 좌석의 통풍 스위치를 누르면 표시등이 켜지면서 해당 좌석에 바람이 나오는 편의장치입니다.").tag(0)
-          
-          OptionModalContentView(hashTags: ["장거리 운전", "다자녀", "안전"], description: "").tag(1)
-          
-          OptionModalContentView(hashTags: ["장거리 운전"], description: "시동이 걸린 상태에서 해당 좌석의 통풍 스위치를 누르면 표시등이 켜지면서 해당 좌석에 바람이 나오는 편의장치입니다. 시동이 걸린 상태에서 해당 좌석의 통풍 스위치를 누르면 표시등이 켜지면서 해당 좌석에 바람이 나오는 편의장치입니다.").tag(2)
+          ForEach(state.components.indices) { i in
+            OptionModalContentView(state: DetailOptionInfo(
+              id: .init(), category: state.components[i].category,
+              containsChoiceCount: state.choiceCount != nil,
+              containsUseCount: state.components[i].useCount != nil,
+              description: state.components[i].description,
+              hashTags: state.components[i].hashTags,
+              hmgData: .init(isOverHalf: state.isOverHalf,
+                             choiceCount: state.choiceCount,
+                             useCount: state.components[i].useCount),
+              image: state.components[i].image,
+              title: state.components[i].name,
+              price: state.price))
+          }
         }
         .onAppear {
           UIScrollView.appearance().isScrollEnabled = true
@@ -39,6 +49,6 @@ struct OptionModalTabContentView: View {
 
 struct OptionModalTabContentView_Previews: PreviewProvider {
     static var previews: some View {
-        OptionModalTabContentView()
+      OptionModalTabContentView( state: .mock())
     }
 }
