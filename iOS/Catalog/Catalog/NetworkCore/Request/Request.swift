166201f707587a536c8c8b8722eb86905e775537
Dohyun Chung <pastapeter@naver.com>
2023-08-22 21:45:06 +0900

    feat: Request Class -> Actor로 변경

--- iOS/Catalog/Catalog/NetworkCore/Request/Request.swift
+++ iOS/Catalog/Catalog/NetworkCore/Request/Request.swift
@@ -7,21 +7,18 @@
 
 import Foundation
 
-class Request {
-
+actor Request {
+  
   var urlRequest: URLRequest
   var retryCount: Int
-  var serialQueue: DispatchQueue = .init(label: "RequestQueue")
-
+  
   init(urlRequest: URLRequest) {
     self.urlRequest = urlRequest
     self.retryCount = 0
   }
-
+  
   func prepareForRetry() {
-    serialQueue.sync { [weak self] in
-      self?.retryCount += 1
-    }
+    self.retryCount += 1
   }
-
+  
 }
