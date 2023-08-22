46e3cf9b9efd40a5b2a18b0f89ee47a91908836f
Lee junho <31722326+plusjob70@users.noreply.github.com>
2023-08-22 09:59:09 +0900

    Feature/#141/handle cors (#247)
    * feat: 허용 Origin 및 메서드 설정
    
    * test: CORS 테스트 생성
    
    * fix: SpringBootTest -> WebMvcTest

---
+++ backend/src/main/java/com/h2o/h2oServer/global/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.h2o.h2oServer.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://h2-cartalog.site", "https://www.h2-cartalog.site")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name());
+    }
+}
