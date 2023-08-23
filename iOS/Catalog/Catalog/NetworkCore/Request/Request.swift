78ddca5e41dbb9cf7a9088bf7dc7dea401fc860b
Dohyun Chung <pastapeter@naver.com>
2023-08-24 02:41:39 +0900

    feat: Actor에서 Class 로 전환하면서 Queue 사용

--- iOS/Catalog/Catalog/NetworkCore/Request/Request.swift
+++ iOS/Catalog/Catalog/NetworkCore/Request/Request.swift
@@ -7,18 +7,21 @@
 
 import Foundation
 
-actor Request {
-  
+class Request {
+
   var urlRequest: URLRequest
   var retryCount: Int
-  
+  var serialQueue: DispatchQueue = .init(label: "RequestQueue")
+
   init(urlRequest: URLRequest) {
     self.urlRequest = urlRequest
     self.retryCount = 0
   }
-  
+
   func prepareForRetry() {
-    self.retryCount += 1
+    serialQueue.sync { [weak self] in
+      self?.retryCount += 1
+    }
   }
-  
+
 }
