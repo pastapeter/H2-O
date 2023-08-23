e33139ed59f670f48053d39991341a1090a58bde
Dohyun Chung <pastapeter@naver.com>
2023-08-24 02:20:19 +0900

    feat: cache Constants 추가하기

---
+++ iOS/Catalog/Catalog/ImageCache/CacheConstants.swift
@@ -0,0 +1,13 @@
+//
+//  CacheConstants.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/24/23.
+//
+
+import Foundation
+
+enum CacheConstants {
+    static let maximumMemoryCacheSize = 52428800
+    static let maximumDiskCacheSize = 52428800
+}
