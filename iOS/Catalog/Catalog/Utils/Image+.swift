b8ff40a20287bc639aac62cd5ee43d57cc46a6b2
Dohyun Chung <pastapeter@naver.com>
2023-08-24 02:23:34 +0900

    feat: Data -> Image SwiftUI

---
+++ iOS/Catalog/Catalog/Utils/Image+.swift
@@ -0,0 +1,38 @@
+//
+//  Image+.swift
+//  Catalog
+//
+//  Created by Jung peter on 8/23/23.
+//
+
+import Foundation
+import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+extension Image {
+  /// Initializes a SwiftUI `Image` from data.
+  init?(data: Data) {
+#if canImport(UIKit)
+    if let uiImage = UIImage(data: data) {
+      self.init(uiImage: uiImage)
+    } else {
+      return nil
+    }
+#elseif canImport(AppKit)
+    if let nsImage = NSImage(data: data) {
+      self.init(nsImage: nsImage)
+    } else {
+      return nil
+    }
+#else
+    return nil
+#endif
+  }
+  
+}
+
+  
