5c406e1a494cce937d0e8ea812ca24c3e30ad540
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-08-24 15:45:33 +0900

    fix: TechnicalSpecDto - Jackson에 의한 역직렬화 처리가 가능하게 하기 위해 생성자 추가 (#315)

--- backend/src/main/java/com/h2o/h2oServer/domain/model_type/dto/TechnicalSpecDto.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/model_type/dto/TechnicalSpecDto.java
@@ -2,18 +2,17 @@ package com.h2o.h2oServer.domain.model_type.dto;
 
 import com.h2o.h2oServer.domain.model_type.Entity.TechnicalSpecEntity;
 import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @ApiModel(value = "차량 성능 정보 조회 응답")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TechnicalSpecDto {
     private Integer displacement;
     private Float fuelEfficiency;
-
-    private TechnicalSpecDto(Integer displacement, Float fuelEfficiency) {
-        this.displacement = displacement;
-        this.fuelEfficiency = fuelEfficiency;
-    }
 
     public static TechnicalSpecDto of(TechnicalSpecEntity technicalSpecEntity) {
         return new TechnicalSpecDto(
