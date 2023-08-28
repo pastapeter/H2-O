137e700461cd5244d68d0b9257dbe71ff7ea7adf
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-08-28 16:56:11 +0900

    Revert "Merge dev-be to main (#343)" (#347)
    This reverts commit 79f63b5326a8006c7a02ee63e1dd4b2227621736.

--- backend/src/main/java/com/h2o/h2oServer/domain/car/exception/NoSuchCarException.java
+++
@@ -1,7 +0,0 @@
-package com.h2o.h2oServer.domain.car.exception;
-
-public class NoSuchCarException extends RuntimeException {
-    public NoSuchCarException() {
-        super("존재하지 않는 차종에 대한 요청입니다.");
-    }
-}
